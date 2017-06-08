678fc877ccdaeafe6bc3516b9fee91a78610cc8e
shcherban <shcherban91@gmail.com>
2017-06-08 19:47:18 +0300

    added macro for low freq

--- Blinkpp/Blinkpp/main.cpp
+++ Blinkpp/Blinkpp/main.cpp
@@ -5,13 +5,13 @@
 
 #define setbit(var,bit) (var |= (0x01 << (bit)))
 #define clrbit(var,bit) (var &= (~(0x01 << (bit))))
+#define set4mhz() CLKPR = 0b10000000; CLKPR = 0b00000001
 
 int main(void)
 {
     asm("nop\n");
 
-    //CLKPR = 0b10000000;
-    //CLKPR = 0b00000001;
+    set4mhz();
 
     DDRB |= 0b00100000;
     while (1)
