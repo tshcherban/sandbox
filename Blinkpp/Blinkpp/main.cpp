ed14af3f6cca9ef028dddf3a26ae72829bc85eda
shcherban <shcherban91@gmail.com>
2017-06-08 19:41:44 +0300

    Used delay from toolchain

--- Blinkpp/Blinkpp/main.cpp
+++ Blinkpp/Blinkpp/main.cpp
@@ -1,40 +1,24 @@
+#define F_CPU 16000000UL
+
 #include <avr/io.h>
+#include <util/delay.h>
+
 #define setbit(var,bit) (var |= (0x01 << (bit)))
 #define clrbit(var,bit) (var &= (~(0x01 << (bit))))
 
 int main(void)
 {
     asm("nop\n");
-    asm("nop\n");
-    asm("nop\n");
-    asm("nop\n");
 
     //CLKPR = 0b10000000;
     //CLKPR = 0b00000001;
-    //const unsigned long delayDuration = 2285713UL; // ~7 cycles per 1 value
-    const unsigned long delayDuration = 228570UL; // ~7 cycles per 1 value
 
     DDRB |= 0b00100000;
     while (1)
     {
         setbit(PORTB, PINB5);
-        for (unsigned long i = 0; i < delayDuration; ++i)
-        {
-            asm("nop\n");
-        }
-        asm("nop\n");
-        asm("nop\n");
-        asm("nop\n");
-        asm("nop\n");
-        
+        _delay_ms(10);
         clrbit(PORTB, PINB5);
-        for (unsigned long i = 0; i < delayDuration; ++i)
-        {
-            asm("nop\n");
-        }
-        asm("nop\n");
-        asm("nop\n");
-        asm("nop\n");
-        asm("nop\n");
+        _delay_ms(10);
     }
 }
