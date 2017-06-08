9af85e7f6b7d8b0f32ee3f48a9086ad1c646e525
shcherban <shcherban91@gmail.com>
2017-06-08 20:33:20 +0300

    frequency test

--- Blinkpp/Blinkpp/main.cpp
+++ Blinkpp/Blinkpp/main.cpp
@@ -1,24 +1,26 @@
 #define F_CPU 16000000UL
+//#define F_CPU 62500UL
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define setbit(var,bit) (var |= (0x01 << (bit)))
 #define clrbit(var,bit) (var &= (~(0x01 << (bit))))
-#define set4mhz() CLKPR = 0b10000000; CLKPR = 0b00000001
+#define set4mhz() CLKPR = 0b10000000; CLKPR = 0b00000010
+#define set62_5khz() CLKPR = 0b10000000; CLKPR = 0b00001000
 
 int main(void)
 {
     asm("nop\n");
 
-    set4mhz();
+    set62_5khz();
 
     DDRB |= 0b00100000;
     while (1)
     {
         setbit(PORTB, PINB5);
-        _delay_ms(10);
+        _delay_us(500);
         clrbit(PORTB, PINB5);
-        _delay_ms(10);
+        _delay_us(500);
     }
 }
