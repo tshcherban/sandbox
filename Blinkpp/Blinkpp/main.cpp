ca0d2d0a6ad5dedfcf4cf81ceeb33907601bc551
shcherban <shcherban91@gmail.com>
2017-06-08 17:40:36 +0300

    Reduced delay

--- Blinkpp/Blinkpp/main.cpp
+++ Blinkpp/Blinkpp/main.cpp
@@ -8,14 +8,11 @@ int main(void)
     asm("nop\n");
     asm("nop\n");
     asm("nop\n");
-    C1 var;
-    if (var.i == 11)
-    {
 
-    }
     //CLKPR = 0b10000000;
     //CLKPR = 0b00000001;
-    const unsigned long delayDuration = 2285713UL;
+    //const unsigned long delayDuration = 2285713UL; // ~7 cycles per 1 value
+    const unsigned long delayDuration = 228570UL; // ~7 cycles per 1 value
 
     DDRB |= 0b00100000;
     while (1)
@@ -25,12 +22,19 @@ int main(void)
         {
             asm("nop\n");
         }
+        asm("nop\n");
+        asm("nop\n");
+        asm("nop\n");
+        asm("nop\n");
         
         clrbit(PORTB, PINB5);
         for (unsigned long i = 0; i < delayDuration; ++i)
         {
             asm("nop\n");
         }
+        asm("nop\n");
+        asm("nop\n");
+        asm("nop\n");
         asm("nop\n");
     }
 }
