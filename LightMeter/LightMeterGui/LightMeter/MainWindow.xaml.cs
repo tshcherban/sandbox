d8606d5f005aed2f8c6f21f2521f357c4b33852a
taras.shcherban <taras.shcherban@eleks.com>
2017-12-07 12:54:42 +0200

    Added signal filtering

--- LightMeter/LightMeterGui/LightMeter/MainWindow.xaml.cs
+++ LightMeter/LightMeterGui/LightMeter/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
 using System.IO.Ports;
 using System.Linq;
@@ -53,16 +54,31 @@ namespace LightMeter
                             }
                         }
                         mst.Seek(0, SeekOrigin.Begin);
-                        var ll = mst.Length;
+
+                        var array = mst.ToArray();
 
                         Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() =>
                         {
                             var m = new PlotModel();
                             var lineSeries = new LineSeries();
-                            int x = 0;
-                            lineSeries.Points.AddRange(mst.ToArray().Skip(1).Select(bt=>new DataPoint(x++, bt)));
+                            var x = 0;
+                            lineSeries.Points.AddRange(array.Skip(1).Select(bt=>new DataPoint(x++, bt)));
                             m.Series.Add(lineSeries);
-                            
+
+                            var series1 = new LineSeries()
+                            {
+                                Color = OxyColor.FromRgb(Colors.Red.R, Colors.Red.G, Colors.Red.B)
+                            };
+
+                            x = 0;
+                            var klm = new Kalman();
+                            foreach (var p in array.Skip(1))
+                            {
+                                var dout = klm.GetValue(p);
+
+                                series1.Points.Add(new DataPoint(x++, dout));
+                            }
+                            m.Series.Add(series1);
                             PlotView1.Model = m;
                         }));
                     }
@@ -75,6 +91,32 @@ namespace LightMeter
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
         {
             p.Write(new byte[]{24}, 0, 1);
+        }
+    }
+
+    public class Kalman
+    {
+        // kalman variables
+        double varVolt = 1e-06;
+        double varProcess = 0.8e-8;
+        double Pc = 0.0;
+        double G = 0.0;
+        double P = 1.0;
+        double Xp = 0.0;
+        double Zp = 0.0;
+        double Xe = 0.0;
+
+        public double GetValue(double value)
+        {
+            // kalman process
+            Pc = P + varProcess;
+            G = Pc / (Pc + varVolt);    // kalman gain
+            P = (1 - G) * Pc;
+            Xp = Xe;
+            Zp = Xp;
+            Xe = G * (value - Zp) + Xp;   // the kalman estimate of the sensor voltage
+
+            return Xe;
         }
     }
 }
